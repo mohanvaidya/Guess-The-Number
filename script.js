6a781bc57740f526057b938ef5ffa48551ff75ab
Mohan Vaidya <131249018+mohanvaidya@users.noreply.github.com>
2024-01-11 11:34:23 +0530

    Update script.js
    Added a message when the player enters a number other than 1-20.

--- script.js
+++ script.js
@@ -28,7 +28,9 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     // document.querySelector('.message').textContent = '⛔️ No number!';
     displayMessage('⛔️ No number!');
-
+  }
+  if (guess < 1 || guess > 20) {
+    displayMessage('Please enter number between 1-20');
     // When player wins
   } else if (guess === secretNumber) {
     // document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -94,18 +96,5 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-});
+};
 
-///////////////////////////////////////
-// Coding Challenge #1
-
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
-
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-GOOD LUCK 😀
-*/
